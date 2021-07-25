627d7fe748887071f35fcc896747be9389b21dfa
Oana Durcau <oana.durcau@outlook.com>
2021-07-25 13:30:08 +0300

    proiect2

--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
@@ -13,10 +13,27 @@ public class ChessBoard {
     }
 
     public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+        //throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+        if (IsLegalBoardPosition(xCoordinate, yCoordinate) && pieces[xCoordinate][yCoordinate] == null) {
+            pawn.setPieceColor(pieceColor);
+            pawn.setXCoordinate(xCoordinate);
+            pawn.setYCoordinate(yCoordinate);
+            pawn.setChessBoard(this);
+            pieces[xCoordinate][yCoordinate] = pawn;
+        }
+        else
+        {
+            pawn.setYCoordinate(-1);
+            pawn.setXCoordinate(-1);
+            pieces[xCoordinate][yCoordinate] = pawn;
+        }
+
     }
 
     public boolean IsLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        //     throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        if (xCoordinate >= 0 && xCoordinate <= 7 && yCoordinate >= 0 && yCoordinate <= 7 && pieces[xCoordinate][yCoordinate] == null)
+            return true;
+        return false;
     }
 }
--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/Pawn.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/Pawn.java
@@ -39,13 +39,38 @@ public class Pawn {
         return this.pieceColor;
     }
 
-    private void setPieceColor(PieceColor value) {
+    public void setPieceColor(PieceColor value) {
         pieceColor = value;
     }
 
     public void Move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.Move()") ;
+
+        //throw new UnsupportedOperationException("Need to implement Pawn.Move()") ;
+         if(movementType == movementType.MOVE)
+        {
+            if(this.getPieceColor() == pieceColor.BLACK)
+            {
+                if( this.getYCoordinate() - newY < 2 || (this.getYCoordinate() == 6 &&  this.getYCoordinate() - newY == 2))
+                {
+                    this.setXCoordinate(newX);
+                    this.setYCoordinate(newY);
+                }
+            }
+
+            if(this.getPieceColor() == pieceColor.WHITE)
+            {
+                if(newY - this.getYCoordinate() < 2 || (this.getYCoordinate() == 1 && newY - this.getYCoordinate() == 2))
+                {
+                    this.setXCoordinate(newX);
+                    this.setYCoordinate(newY);
+                }
+            }
+            
+        }
+
+
     }
+
 
     @Override
     public String toString() {
@@ -53,7 +78,7 @@ public class Pawn {
     }
 
     protected String CurrentPositionAsString() {
-        String eol = System.lineSeparator();
+        java.lang.String eol = System.lineSeparator();
         return String.format("Current X: {1}{0}Current Y: {2}{0}Piece Color: {3}", eol, xCoordinate, yCoordinate, pieceColor);
     }
 }
